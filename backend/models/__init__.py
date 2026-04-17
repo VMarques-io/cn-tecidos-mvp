f8971e90f55073ab710351efe4894133ef5aad5b
Vitor Marques <145176163+VMarques-io@users.noreply.github.com>
2026-04-17 11:12:25 -0300

    Update exports to include new models

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -3,5 +3,7 @@
 from .user import User
 from .conversation import Message
 from .flow_state import FashionFlowState
+from .store_info import StoreInfo
+from .product import Product
 
-__all__ = ["User", "Message", "FashionFlowState"]
+__all__ = ["User", "Message", "FashionFlowState", "StoreInfo", "Product"]
